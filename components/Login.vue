d2ac2e21eb0b1ccdf7e610e4f81520ec8e4f513f
javimosch <arancibiajav@gmail.com>
2024-05-06 13:22:03 +0200

    Initial commit

---
+++ components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="bg-white dark:bg-black min-h-screen flex items-center justify-center"
+  >
+    <form class="bg-white dark:bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label
+          class="block text-black dark:text-white text-sm font-bold mb-2"
+          for="username"
+        >
+          Username
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-black dark:text-white leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+      <div class="mb-6">
+        <label
+          class="block text-black dark:text-white text-sm font-bold mb-2"
+          for="password"
+        >
+          Password
+        </label>
+        <input
+          class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-black dark:text-white mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-black hover:bg-zinc-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          @click="$emit('onLoginClick')"
+        >
+          Sign In
+        </button>
+        <a
+          class="inline-block align-baseline font-bold text-sm text-black dark:text-white hover:text-blue-800"
+          href="#"
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Login @onLoginClick="onLoginClick" />
+</template>
+<script setup lang="ts">
+const name = ref('Foo');
+const { onLoginClick } = useCanLogin();
+const backendURL = 'https://b3adf14e3b3e23b7e291f8b3426938cd.serveo.net/';
+</script>
